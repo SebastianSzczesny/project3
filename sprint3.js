8b5b2047d30932132c5848825838b10b360bba49
SebastianSzczesny <151460995+SebastianSzczesny@users.noreply.github.com>
2023-11-30 17:23:13 +0100

    Add files via upload

---
+++ sprint3.js
@@ -0,0 +1,123 @@
+// 1a
+let number = 10;
+// 1b
+let name = "sebastian";
+// 1c
+let none;
+none = "xyz";
+// 1d
+const obj = ["name", "number"];
+// 1e
+let a = true;
+const b = false;
+// 1f
+let numberA = 10;
+let numberB = 20
+let suma = numberA + numberB;
+// 1g
+let nameSurname = "Sebastian Szczesny";
+// 1h
+let number2 = 2.25;
+// 1i
+let wynik = number / 5;
+// 1j
+let city = "warszawa";
+
+// 2a
+let table = ["jeden", 1, true];
+// 2b
+let car = {
+     make: opel,
+     model: astra,
+     year: 2000,
+}
+// 2c
+let numbers = [1, 2, 3];
+// 2d
+let person = {
+     age: 20,
+     name2: "kris",
+     isStudent: false,
+}
+// 2e
+let empty = {};
+empty.color = "blue";
+// 2f
+let animals [
+     {name: "dog", age: 5}
+     {name: "cat", age: 7}
+]
+// 2g
+let array = [true, false, true, false]
+// 2h
+let student = {
+     name: "Greg",
+     grades: [1, 2, 3, 4, 5, 6],
+     isActive: true,
+}
+// 2i
+let DifObj = [false, 20, {name: "adam", surname: "tommy"}]
+// 2j
+let book = {
+     name: "harry potter";
+     year: 1999;
+     author: "J K ROWLING"
+}
+// 3a
+let number = 5
+if (number < 10) {
+     console.log("numer jest mniejszy niż 10")
+}
+// 3b
+let text = "hello"
+if (text === "hello"){
+     console.log("text = hello")
+}
+// 3c
+let hight
+hight > 180 ? console.log("wysoki") : console.log("niski");
+// 3d
+let age = 20;
+if (age >= 18){
+     console.log("wiek jest równy/większy niż 18")
+} else {
+     console.log("wiek jest mniejszy niż 18")
+}
+// 3e
+let table = []
+if (table.lenght === 0) {
+     console.log("tablica jest pusta") 
+} else {
+     console.log("tablica nie jest pusta")
+}
+// 3f
+let isMember = true;
+let discount = (isMember = true) ? discount = true : discount = false;
+// 3g
+let something = "symbol"
+if (something.lenght >= 5) {
+     console.log("długość ciągu znaków w zmiennej jest większa niż 5")
+} else {
+     console.log("długość ciągu znaków w zmiennej nie jest większa niż 5")
+}
+// 3h
+let number = 10
+if (number % 2 === 0) {
+     console.log("liczba jest parzysta")
+} else {
+     console.log("liczba jest nieparzysta")
+}
+// 3i
+let gender = "male";
+   gander === "male" ? console.log("mężczyzna") : console.log("kobieta");
+//    3j
+let student = { type: "mgr", year: 1 };
+if (student.type = "mgr") {
+  console.log("studia mgr");
+} else {
+  console.log("studia lic");
+}
+
+
+
+
